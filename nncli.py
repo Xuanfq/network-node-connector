1d04d4a6d3975fa1bc550187885805fc0237a8d4
Xuanfq <2624208682@qq.com>
2025-08-03 21:07:36 +0800

    push the remain code

--- nncli.py
+++ nncli.py
@@ -14,6 +14,16 @@ parser.add_argument(
     help="show version",
 )
 
+subparser = parser.add_subparsers()
+
+parser_server = subparser.add_parser("server", help="server command")
+server_action_group = parser_server.add_mutually_exclusive_group(required=True)
+server_action_group.add_argument("--start", action="store_true")
+server_action_group.add_argument("--stop", action="store_true")
+server_action_group.add_argument("--status", action="store_true")
+server_action_group.add_argument("--restart", action="store_true")
+server_action_group.add_argument_group()
+
 
 def main():
     args = parser.parse_args()
